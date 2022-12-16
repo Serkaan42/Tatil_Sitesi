447b8712e832492f30d6abfea84f4fc8babe26bc
Serkan <ipeks8926@gmail.com>
2022-12-16 13:54:04 +0300

    Add project files.

---
+++ TravelTripProje/Models/Siniflar/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Context :DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }    
+        public DbSet<Addres> AdressBlogs { get; set; }     
+        public DbSet<Blog> Blogs { get; set; }    
+        public DbSet<Hakkimizda> Hakkimizdas { get; set; }    
+        public DbSet<Iletisim> Iletisims { get; set; }    
+        public DbSet<Yorumlar> Yorumlars { get; set; }    
+
+        //Ders 9 da kaldım.
+        
+    }
+}
